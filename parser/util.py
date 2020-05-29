0ab85b75c8e46508e0a1557121f137d4d673a249
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2020-05-29 18:01:17 +0400

    убрал лишнее

--- parser/util.py
+++ parser/util.py
@@ -1,24 +1,12 @@
 # coding: utf8
-import aiofiles
 # этот файл содержит вспомогательные функции
-
-# Убирает формат и ставит вместо него png
-def convertNameToPng(image_name):
-    replaceAfter(image_name, '.', 'png')
+import aiofiles
 
 def replaceAfter(string:str, symbol:str, replace_with:str):
     i = 0
     while(string[-i]!=symbol):
         i+=1
     return string[:-i+1] + replace_with
-
-# Вычленяет расширение картинки из её url
-def getExtention(url):
-    return getLastFrom(url, '.')
-
-# Вычленяет имя картинки из её url
-def getName(url):
-    return getLastFrom(url, '/')
 
 # Выдаёт подстроку после последнего вхождения данного символа
 def getLastFrom(string:str, symbol: str):
@@ -33,13 +21,6 @@ def cutLastFrom(string:str, symbol: str):
     while(string[i]!=symbol):
         i-=1
     return string[:i]
-
-def readFile(filename):
-    res = []
-    with open(filename, "r") as f:
-        for line in f:
-            res.append(line)
-    return res
 
 async def append_to_file(arr, filename: str):
     async with aiofiles.open(filename, "a") as f:
