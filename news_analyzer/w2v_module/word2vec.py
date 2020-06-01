c4d950c293705bedcc5e824d033e1bcd1eebfdc5
Nikolay <kolan727@gmail.com>
2020-06-01 20:37:48 +0400

    реализовал обучение модели word2vec

---
+++ news_analyzer/w2v_module/word2vec.py
@@ -0,0 +1,95 @@
+PATH = 'data/*.txt'
+
+from pyspark.sql import SparkSession
+from pyspark.ml.feature import Tokenizer
+from pyspark.ml.feature import StopWordsRemover
+from pyspark.ml.feature import Word2Vec
+import re
+import string
+import datetime
+
+
+def remove_punctuation(text):
+    """
+    Удаление пунктуации из текста
+    """
+    return text.translate(str.maketrans('', '', string.punctuation))
+
+
+def get_only_words(tokens):
+    """
+    Получение списка токенов, содержащих только слова
+    """
+    return list(filter(lambda x: re.match('[а-яА-Я]+', x), tokens))
+
+
+def create_w2v_model():
+    spark = SparkSession \
+        .builder \
+        .appName("SimpleApplication") \
+        .getOrCreate()
+
+    input_file = spark.sparkContext.wholeTextFiles(PATH)
+
+    print("""
+    
+    Подготовка данных (1)...
+    
+    """)
+    prepared_data = input_file.map(lambda x: (x[0], remove_punctuation(x[1])))
+
+    print("""
+    
+    Подготовка данных (2)...
+    
+    """)
+    df = prepared_data.toDF()
+
+    print("""
+    
+    Подготовка данных (3)...
+    
+    """)
+    prepared_df = df.selectExpr('_2 as text')
+
+    print("""
+    
+    Разбитие на токены...
+    
+    """)
+    tokenizer = Tokenizer(inputCol='text', outputCol='words')
+    words = tokenizer.transform(prepared_df)
+
+    print("""
+    
+    Очистка от стоп-слов...
+    
+    """)
+    stop_words = StopWordsRemover.loadDefaultStopWords('russian')
+    remover = StopWordsRemover(inputCol="words", outputCol="filtered", stopWords=stop_words)
+    filtered = remover.transform(words)
+
+    print("""
+    
+    Построение модели...
+    
+    """)
+    word2Vec = Word2Vec(inputCol='words', outputCol='result')
+    model = word2Vec.fit(words)
+    w2v_df = model.transform(words)
+
+    print("""
+    
+    Сохранение модели...
+    
+    """)
+    today = datetime.datetime.today()
+    model_name = today.strftime("model/kurs_model")
+    print("""
+    
+    Model  """ + model_name + """  saved
+    
+    """)
+    model.save(model_name)
+
+    spark.stop()
