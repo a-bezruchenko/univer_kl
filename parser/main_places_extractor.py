240d329444986e9f57c1a069fcdcef24dceeca25
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2020-06-01 18:13:14 +0400

    Добавил извлечение мест

---
+++ parser/main_places_extractor.py
@@ -0,0 +1,15 @@
+import db
+from parser_funcs import parse_html
+
+if __name__ == "__main__":
+    with open("data/places.html", "r", encoding="utf-8") as f:
+        text = f.read()
+    parsed_content = parse_html(text)
+    ta = parsed_content.find("div", id="afisha-content").findAll("div", class_="ta-f-box ta-f-box-cat")
+    places = list(map(
+            lambda x: x.findAll("a")[1].text,
+            parsed_content.find("div", id="afisha-content").findAll("div", class_="ta-f-box ta-f-box-cat")))
+    con = db.init_sync()
+    cur = con.cursor()
+    for place in places:
+        cur.execute("INSERT INTO places (name) VALUES (%s);", (place))
--- parser/safe_get.py
+++ parser/safe_get.py
@@ -1,7 +1,12 @@
 # coding: utf8
-from time import time as current_time
 import asyncio
 from aiohttp import ClientSession
+
+from time import time as current_time
+from time import sleep
+import requests
+
+from fake_useragent import UserAgent
 
 # файл для запросов к серверу
 
@@ -11,10 +16,6 @@ SAFE_GET_LOCK = asyncio.Lock()
 
 
 async def fetch_html(url: str, session: ClientSession, **kwargs) -> str:
-    """
-    GET запрос оболочки для загрузки страницы HTML.
-    kwargs передаются в session.request().
-    """
     resp = await safe_get(url, session, **kwargs)
     html = await resp.text()
     return html
@@ -33,5 +34,16 @@ async def safe_get(url, session, wait_time=DEFAULT_WAIT_TIME, **kwargs):
         #print(sleep_time)
         await asyncio.sleep(sleep_time)
     print(str.format("Запрашиваем {0}, время:{1}", url, current_time()))
-    return await session.request(method="GET", url=url, raise_for_status=True,**kwargs)
+    return await session.request(method="GET", url=url, raise_for_status=True, headers={'User-Agent': UserAgent().chrome}, **kwargs)
 
+def fetch_html_sync(url: str, **kwargs) -> str:
+    resp = safe_get_sync(url, **kwargs)
+    html = resp.text()
+    return html
+
+def safe_get_sync(url, wait_time=DEFAULT_WAIT_TIME):
+    global LAST_REQUEST_TIME
+    if "LAST_REQUEST_TIME" in globals() and current_time() - LAST_REQUEST_TIME < wait_time:
+        LAST_REQUEST_TIME = current_time() + LAST_REQUEST_TIME
+        sleep(current_time() - LAST_REQUEST_TIME)
+    return requests.get(url=url, headers={'User-Agent': UserAgent().chrome})
