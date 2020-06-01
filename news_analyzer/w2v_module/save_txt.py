d13899e763bc17c13ad728619485186314977505
Nikolay <kolan727@gmail.com>
2020-06-01 20:36:58 +0400

    реализовал функцию взятия статей из бд и сохранения в .txt файлы

---
+++ news_analyzer/w2v_module/save_txt.py
@@ -0,0 +1,15 @@
+def save_text_db_to_txt(con):
+    filename = "text"
+    i = 1
+
+    with con:
+        cur = con.cursor()
+        # Берем тексты статей из БД
+        cur.execute("SELECT text FROM storage WHERE text IS NOT NULL")
+
+        # Сохраняем в файлы для обучения модели
+        for el in cur.fetchall():
+            output_file = open(f'./data/{filename}_{i}.txt', 'w')
+            i += 1
+            output_file.write(el[0])
+            output_file.close()
