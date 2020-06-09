f6568617fc76e3ae157fe8301bb09d7031ac7f22
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2020-06-09 15:09:44 +0400

    переходы

--- web_interface/app.py
+++ web_interface/app.py
@@ -6,15 +6,30 @@ from db_init import *
 
 app = Flask(__name__)
 
+db_con = init_sync()
 
 @app.route('/')
 def index():
-    db_con = init_sync()
     with db_con:
         cur = db_con.cursor()
         # Берем статьи из БД
         cur.execute("SELECT * FROM storage WHERE text IS NOT NULL")
         return render_template('index.html', articles=cur.fetchall())
+
+
+@app.route('/getTonality')
+def get_tonality():
+    return "Здесь будет тональность"
+
+
+@app.route('/getFacts')
+def get_facts():
+    return "Здесь будет результат томиты"
+
+
+@app.route('/getSynonyms')
+def get_synonyms():
+    return "Здесь будут синонимы"
 
 
 if __name__ == "__main__":
