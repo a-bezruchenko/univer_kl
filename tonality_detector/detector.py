43da0aaf29db7e340748ae9c8120ca02915b56e7
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2020-06-12 14:53:38 +0400

    завершение работы с анализатором тональности

--- tonality_detector/detector.py
+++ tonality_detector/detector.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from dostoevsky.tokenization import RegexTokenizer
 from dostoevsky.models import FastTextSocialNetworkModel
 import sys
@@ -19,26 +17,45 @@ from db_init import *
 #     '''Замечательный телефон, пользуюсь им уже 2 года, очень нравится!''']
 
 
+def insert_tonality(con, id_filtered, tonality):
+    with con.cursor() as cur:
+        cur.execute("INSERT INTO tonality (id_filtered, first_tonality, "
+                    "first_value, second_tonality, second_value) VALUES (%s, %s, %s, %s, %s);",
+                    (id_filtered, tonality[0][0], float(tonality[0][1]),
+                     tonality[1][0], float(tonality[1][1])))
+
+
+def print_tonality(text, tonality):
+    print(text + "\nТональность текста:")
+    for ton in tonality:
+        print(ton)
+
+
 def main():
     db_con = init_sync()
     tokenizer = RegexTokenizer()
     model = FastTextSocialNetworkModel(tokenizer=tokenizer)
-    sentences = []
-    i = 0
     with db_con:
         cur = db_con.cursor()
         # Запрос к предложениям выдленным томитой
-        cur.execute("SELECT text FROM filtered WHERE text IS NOT NULL")
+        cur.execute("SELECT id, text FROM filtered WHERE text IS NOT NULL")
 
-        for sentence in cur.fetchall():
-            if len(sentence[0]) > 0:
-                sentences.append(sentence[0])
+        filtreds = [{x[0]: x[1]} for x in cur.fetchall() if len(x[1]) > 0]
+        tonality = []
 
-        results = model.predict(sentences, k=2)
-        for i, el in enumerate(zip(sentences, results)):
-            token, sentiment = el
-            print(sentences[i])
-            pprint({word: key for key, words in sentiment.items() for word in words})
+
+        for filtred in filtreds:
+            results = model.predict(filtred.values(), k=2)
+            for i, el in enumerate(zip(filtred.values(), results)):
+                token, sentiment = el
+                tonality.clear()
+                for key, value in filtred.items():
+                    id_filtered = key
+                    text = value
+                for item in sentiment.items():
+                    tonality.append(item)
+                insert_tonality(db_con, id_filtered, tonality)
+                print_tonality(text, tonality)
 
 
 if __name__ == '__main__':
