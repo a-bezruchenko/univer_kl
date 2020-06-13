c9b4c93c15b37833542bd56ae7ca799497daa20e
tankistqazwsx <alexei.bezruchenko@yandex.ru>
2020-06-13 11:59:36 +0400

    ускорил томиту где-то в 5-10 раз

--- news_analyzer/tomita_module/find_facts.py
+++ news_analyzer/tomita_module/find_facts.py
@@ -1,3 +1,4 @@
+# coding: utf8
 import subprocess
 import os
 import re
@@ -5,8 +6,8 @@ import sys
 from pprint import pprint
 from db_init import init_sync
 
-def find_facts(text):
-    raw_result = analyse_text_with_tomita(text)
+def find_facts(text, base_path = "./", path_to_tomita = "./tomita-parser"):
+    raw_result = analyse_text_with_tomita(text, base_path=base_path, path_to_tomita=path_to_tomita)
     res = []
     regexp = r"(.*)\n\s+Person\n\s+\{((?:\n\s+\s+(?:Name|FamilyName|FatherName) = .*?)+\n\s+\})"
     for find_res in re.findall(regexp, raw_result):
@@ -61,21 +62,47 @@ TTextMinerConfig {
     return output
     
 if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        PATH_TO_TOMITA = "./tomita-parser"
+        BASE_PATH = "./"
+    elif len(sys.argv) == 2:
+        PATH_TO_TOMITA = sys.argv[1]
+        BASE_PATH = "./"
+    else:
+        PATH_TO_TOMITA = sys.argv[1]
+        BASE_PATH = sys.argv[2]
+    print("Путь к томите: " + PATH_TO_TOMITA)
+    print("Путь к файлам грамматики: " + BASE_PATH)
     con = init_sync()
     print("Начинаю обрабатывать...")
     with con.cursor() as cur:
         cur.execute(f'SELECT count(*) FROM storage;')
         db_size = cur.fetchall()[0][0]
-        batch_size = 1
-        for offset in [x for x in range(0, db_size-db_size%batch_size, batch_size)]:
+        batch_size = 50
+        for offset in range(0, db_size-db_size%batch_size, batch_size):
             cur.execute(f'SELECT link, text FROM storage LIMIT {batch_size} OFFSET {offset};')
-            for row in cur.fetchall():
-                link, text = row
-                res = find_facts(text)
-                if res != []:
-                    for el in res:
-                        el = str(el)
-                        if len(el) > 0:
-                            cur.execute('INSERT INTO filtered (link, text) values (%s, %s);', (link, el))
-                print(f"{link} обработана")
+            rows = cur.fetchall()
+            text = '\n'.join(list(map(lambda x:x[1], rows)))
+            res = find_facts(text, base_path=BASE_PATH, path_to_tomita=PATH_TO_TOMITA)
+            if res != []:
+                for el in res:
+                    el = str(el)
+                    el = (el.replace(" .", ".")
+                        .replace(" ,", ",")
+                        .replace(" ;", ";")
+                        .replace(" :", ":")
+                        .replace(" -", "-")
+                        .replace(" —", "—"))
+                    link_to_insert = ""
+                    for row in rows:
+                        link, text = row
+                        if text.replace("«", '"').replace("»", '"').replace(" ", "").find(el.replace(" ", "")) != -1:
+                            link_to_insert = link
+                            break
+                    if len(el) > 0 and link_to_insert != "":
+                        cur.execute('INSERT INTO filtered (link, text) values (%s, %s);', (link_to_insert, el))
+                    elif link_to_insert == "":
+                        print("Не удалось найти статью:")
+                        print(el)
+            print(f"Первые {offset+batch_size} записей обработаны")
     print("Данные обработаны")
