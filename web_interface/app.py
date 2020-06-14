0df13e4f214f39027b77b03a224c8dc9b3ca498f
Kolyamba-mamba <mr.kolyamba99@yandex.ru>
2020-06-14 13:38:40 +0400

    реализация пагинации

--- web_interface/app.py
+++ web_interface/app.py
@@ -1,5 +1,6 @@
 import sys
 from flask import Flask, render_template, request
+from flask_paginate import Pagination
 
 sys.path.append("../parser/")
 from db_init import *
@@ -11,19 +12,37 @@ db_con = init_sync()
 @app.route('/')
 def index():
     with db_con:
+        page = int(request.args.get('page', 1))
+        per_page = 100
+        offset = (page - 1) * per_page
+
         cur = db_con.cursor()
+        cur.execute("SELECT count(*) FROM storage WHERE text IS NOT NULL ")
+        size = cur.fetchone()[0]
+
         # Берем статьи из БД
-        cur.execute("SELECT * FROM storage WHERE text IS NOT NULL ORDER BY date DESC LIMIT 100")
-        return render_template('index.html', articles=cur.fetchall())
+        cur.execute("SELECT * FROM storage WHERE text IS NOT NULL ORDER BY `date` DESC LIMIT %s OFFSET %s",
+                    (per_page, offset))
+        pagination = Pagination(page=page, per_page=per_page, total=size, css_framework='bootstrap4')
+        return render_template('index.html', articles=cur.fetchall(), page=page, per_page=per_page, pagination=pagination)
 
 
 @app.route('/getTonality')
 def get_tonality():
     with db_con:
+        page = int(request.args.get('page', 1))
+        per_page = 200
+        offset = (page - 1) * per_page
+
         cur = db_con.cursor()
+        cur.execute("SELECT count(*) FROM tonality")
+        size = cur.fetchone()[0]
+
         cur.execute("SELECT f.text, t.first_tonality, t.first_value, t.second_tonality, t.second_value "
-                    "FROM `tonality` t INNER JOIN `filtered` f ON f.id = t.id_filtered")
-        return render_template('tonality.html', elements=cur.fetchall())
+                    "FROM `tonality` t INNER JOIN `filtered` f ON f.id = t.id_filtered LIMIT %s OFFSET %s",
+                    (per_page, offset))
+        pagination = Pagination(page=page, per_page=per_page, total=size, css_framework='bootstrap4')
+        return render_template('tonality.html', elements=cur.fetchall(), age=page, per_page=per_page, pagination=pagination)
 
 
 @app.route('/getSynonyms', methods=['post', 'get'])
